7285084324f38c6602f94a738e848cb89032f0aa
c2061455 <okeefesj@cardiff.ac.uk>
2022-03-25 16:48:18 +0000

    Can search for book title and return first result

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/HomeFragment.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/HomeFragment.java
@@ -10,7 +10,18 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomeFragment extends Fragment {
 
@@ -28,11 +39,43 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstance) {
         SearchView searchView = (SearchView) getView().findViewById(R.id.searchView);
 
+
         //Access text from search view
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
+
                 Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
+                String API_KEY = "AIzaSyDbNPjEszabwPq-xMd3sNTUIaXp9A5IbDA";
+                String url = "https://www.googleapis.com/books/v1/volumes?q=";
+                RequestQueue requestQueue = Volley.newRequestQueue(getContext()); //getContext() might be wrong
+                JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, "https://www.googleapis.com/books/v1/volumes?q=" + s, null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    System.out.println(response.getJSONArray("items").getJSONObject(1).getJSONObject("volumeInfo").getString("title"));
+                                    System.out.println(response.getJSONArray("items").getJSONObject(1).getJSONObject("volumeInfo").getString("authors"));
+                                    String titleResponse =  response.getJSONArray("items").getJSONObject(1).getJSONObject("volumeInfo").getString("title");
+                                    String authorResponse =  response.getJSONArray("items").getJSONObject(1).getJSONObject("volumeInfo").getString("authors");
+                                    System.out.println("title: " + titleResponse);
+                                    System.out.println("author: " + authorResponse);
+                                    TextView title = (TextView) getView().findViewById(R.id.textView2);
+                                    TextView author = (TextView) getView().findViewById(R.id.textView3);
+                                    title.setText(titleResponse);
+                                    author.setText(authorResponse);
+                                } catch (JSONException err) {
+                                    err.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                            }
+                        }
+                );
+                requestQueue.add(getRequest);
                 return false;
             }
 
