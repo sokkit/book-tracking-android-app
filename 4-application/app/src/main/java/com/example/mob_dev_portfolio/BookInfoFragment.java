3260ae2c373d2f361ce4880eb28bfde0dfabfa12
c2061455 <okeefesj@cardiff.ac.uk>
2022-04-06 16:43:49 +0100

    added quote edit text box

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/BookInfoFragment.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/BookInfoFragment.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -46,6 +47,7 @@ public class BookInfoFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_book_info, container, false);
 
